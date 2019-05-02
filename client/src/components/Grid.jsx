49919910b4152bff509a910b94084137e4f48583
rotimi-babalola <rotimi.babalola@andela.com>
2019-05-02 22:41:48 +0100

    feature: add cropped image

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -4,15 +4,22 @@ import '../styles/scss/grid.scss';
 
 const Grid = () => (
   <div className="container">
-    <div className="cell cell1" />
-    <div className="cell cell2" />
-    <div className="cell cell3" />
-    <div className="cell cell4" />
-    <div className="cell cell5" />
-    <div className="cell cell6" />
-    <div className="cell cell7" />
-    <div className="cell cell8" />
-    <div className="cell cell-empty" />
+    <div className="cell cell1">0</div>
+    <div className="cell cell2">1</div>
+    <div className="cell cell3">2</div>
+    <div className="cell cell4">3</div>
+    <div className="cell cell5">4</div>
+    <div className="cell cell6">5</div>
+    <div className="cell cell7">6</div>
+    <div className="cell cell8">7</div>
+    <div className="cell cell9">8</div>
+    <div className="cell cell10">9</div>
+    <div className="cell cell11">10</div>
+    <div className="cell cell12">11</div>
+    <div className="cell cell13">12</div>
+    <div className="cell cell14">13</div>
+    <div className="cell cell15">14</div>
+    <div className="cell cell16">15</div>
   </div>
 );
 
