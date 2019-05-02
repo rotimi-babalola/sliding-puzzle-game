b58cbcc324a8c6c3a9adf055c6982482232f48ed
rotimi-babalola <rotimi.babalola@andela.com>
2019-05-02 17:04:32 +0100

    feature: add Grid component

---
+++ client/src/components/Grid.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../styles/scss/grid.scss';
+
+const Grid = () => (
+  <div className="container">
+    <div className="cell" />
+    <div className="cell" />
+    <div className="cell" />
+    <div className="cell" />
+    <div className="cell" />
+    <div className="cell" />
+    <div className="cell" />
+    <div className="cell" />
+  </div>
+);
+
+export default Grid;
--- client/src/components/Puzzle.jsx
+++ client/src/components/Puzzle.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Grid from './Grid';
 
 import '../styles/scss/puzzle.scss';
 
 const Puzzle = () => (
   <div className="puzzle">
     <h1 className="heading">Sliding Puzzle Game</h1>
+    <Grid />
   </div>
 );
 
