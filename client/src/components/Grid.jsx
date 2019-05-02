b134ca7d77d5d253924669314e9c9877a59b1ed4
rotimi-babalola <rotimi.babalola@andela.com>
2019-05-02 17:29:55 +0100

    feature: show grid in image

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -4,14 +4,15 @@ import '../styles/scss/grid.scss';
 
 const Grid = () => (
   <div className="container">
-    <div className="cell" />
-    <div className="cell" />
-    <div className="cell" />
-    <div className="cell" />
-    <div className="cell" />
-    <div className="cell" />
-    <div className="cell" />
-    <div className="cell" />
+    <div className="cell cell1" />
+    <div className="cell cell2" />
+    <div className="cell cell3" />
+    <div className="cell cell4" />
+    <div className="cell cell5" />
+    <div className="cell cell6" />
+    <div className="cell cell7" />
+    <div className="cell cell8" />
+    <div className="cell cell-empty" />
   </div>
 );
 
