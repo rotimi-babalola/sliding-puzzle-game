49f84ee6b4dbaf85aff461f9375a28651e6996ab
rotimi-babalola <rotimi.babalola@andela.com>
2019-05-04 14:08:10 +0100

    add unit tests for puzzle reducer

---
+++ client/src/__tests__/puzzle.reducer.test.js
@@ -0,0 +1,39 @@
+import expect from 'expect';
+import { range } from 'lodash';
+
+import puzzleReducer from '../reducers/puzzle.reducer';
+import { shuffleNumbers, isSolved } from '../utils';
+import { SHUFFLE, SWAP, EMPTY_TILE } from '../constants';
+
+const initialState = {
+  puzzleNumbers: shuffleNumbers(range(0, 16)),
+  isSolved: false,
+  sortedNumbers: range(0, 16),
+};
+
+const action = {};
+
+describe('Puzzle reducer', () => {
+  it('should return initial state when there is no action', () => {
+    expect(puzzleReducer(initialState, action)).toEqual(initialState);
+  });
+
+  it('should handle SHUFFLE', () => {
+    const actionCopy = { ...action, type: SHUFFLE };
+    expect(
+      isSolved(puzzleReducer(initialState, actionCopy).puzzleNumbers),
+    ).toEqual(false);
+
+    expect(
+      puzzleReducer(initialState, actionCopy).puzzleNumbers.indexOf(EMPTY_TILE),
+    ).toEqual(initialState.puzzleNumbers.length - 1);
+  });
+
+  it('should handle SWAP', () => {
+    const actionCopy = { ...action, type: SWAP, payload: 14 };
+
+    expect(
+      puzzleReducer(initialState, actionCopy).puzzleNumbers.indexOf(EMPTY_TILE),
+    ).toEqual(actionCopy.payload);
+  });
+});
